8f321cc087cb6a68c50c0c5b369e7eaade732f88
gorozcoua <g.orozcos@uniandes.edu.co>
2025-04-26 14:42:58 -0500

    Test Negativo Albumes

---
+++ app/src/androidTest/java/com/example/vinilos/ui/AlbumNoNetworkTest.kt
@@ -0,0 +1,78 @@
+package com.example.vinilos.ui
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.vinilos.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class AlbumNoNetworkTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun albumNoNetworkTest() {
+        val materialButton = onView(
+            allOf(
+                withId(R.id.btn_albums), withText("Álbumes"),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        Thread.sleep(1000)
+
+        val textView = onView(
+            allOf(
+                withId(R.id.error_message), withText("Network Error"),
+                withParent(withParent(withId(R.id.fragment_container))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Network Error")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
