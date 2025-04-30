7e573cddcc411308fccb14e90a7c9d4036597389
gorozcoua <g.orozcos@uniandes.edu.co>
2025-04-30 18:30:37 -0500

    Include deezer API

--- app/src/main/java/com/example/vinilos/repositories/AlbumRepository.kt
+++ app/src/main/java/com/example/vinilos/repositories/AlbumRepository.kt
@@ -3,11 +3,11 @@ package com.example.vinilos.repositories
 import android.app.Application
 import com.android.volley.VolleyError
 import com.example.vinilos.models.Album
-import com.example.vinilos.network.NetworkServiceAdapter
+import com.example.vinilos.network.DeezerServiceAdapter
 
 class AlbumRepository (val application: Application){
     fun refreshData(callback:(resp:List<Album>)->Unit, onError: (error: VolleyError)->Unit) {
-        NetworkServiceAdapter.getInstance(application).getAlbums({
+        DeezerServiceAdapter.getInstance(application).getAlbums({
             callback(it)
         },
             onError
